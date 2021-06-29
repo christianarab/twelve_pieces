01a7550abc62383ac406862735d387323a22efe9
christian <rewindforward@gmail.com>
2021-06-29 10:33:54 -0400

    Fixes restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,6 +9,7 @@ class RestaurantsController < ApplicationController
 
   # GET /restaurants/1 or /restaurants/1.json
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   # GET /restaurants/new
